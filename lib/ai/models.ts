4d3319a0c1ab4656a332713cf1d6af1b6bb1b404
ndang <ndang2319@gmail.com>
2025-03-05 06:51:08 +0700

    feat: init project

---
+++ lib/ai/models.ts
@@ -0,0 +1,51 @@
+import { openai } from '@ai-sdk/openai';
+import { fireworks } from '@ai-sdk/fireworks';
+import { google } from '@ai-sdk/google';
+import {
+  customProvider,
+  extractReasoningMiddleware,
+  wrapLanguageModel,
+} from 'ai';
+
+export const DEFAULT_CHAT_MODEL: string = 'chat-model-small';
+
+export const myProvider = customProvider({
+  languageModels: {
+    'chat-model-small': openai('gpt-4o-mini'),
+    'chat-model-large': openai('gpt-4o'),
+    'chat-model-reasoning': wrapLanguageModel({
+      model: fireworks('accounts/fireworks/models/deepseek-r1'),
+      middleware: extractReasoningMiddleware({ tagName: 'think' }),
+    }),
+    'google-model-small': google('gemini-1.5-flash-latest'),
+    'google-model-large': google('gemini-2.0-flash-exp'),
+  },
+  // imageModels: {
+  //   'small-model': openai.image('dall-e-2'),
+  //   'large-model': openai.image('dall-e-3'),
+  // },
+});
+
+interface ChatModel {
+  id: string;
+  name: string;
+  description: string;
+}
+
+export const chatModels: Array<ChatModel> = [
+  {
+    id: 'chat-model-small',
+    name: 'Small model',
+    description: 'Small model for fast, lightweight tasks',
+  },
+  {
+    id: 'chat-model-large',
+    name: 'Large model',
+    description: 'Large model for complex, multi-step tasks',
+  },
+  {
+    id: 'chat-model-reasoning',
+    name: 'Reasoning model',
+    description: 'Uses advanced reasoning',
+  },
+];
